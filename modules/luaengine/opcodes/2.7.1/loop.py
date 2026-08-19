2c2edc5c24fff2d0321fde1768e6ce81a34bd7cb
axom tools <adamowaissi055@gmail.com>
2026-08-20 01:00:12 +0300

    Create loop.py

---
+++ modules/luaengine/opcodes/2.7.1/loop.py
@@ -0,0 +1,16 @@
+forloop = {
+    "String": """local A = Inst[OP_A];
+local Step = Stk[A + 2];
+local Index = Stk[A] + Step;
+Stk[A] = Index;
+if (Step > 0) then 
+if (Index <= Stk[A+1]) then
+InstrPoint = Inst[OP_B];
+Stk[A+3] = Index;
+end
+elseif (Index >= Stk[A+1]) then
+InstrPoint = Inst[OP_B];
+Stk[A+3] = Index;
+end""",
+    "Create": lambda inst: inst.update({"B": inst["B"] - inst["PC"] - 1}) or inst
+}
