771aa61b32d7f5e670712e44ab9372ebf2bd03a4
axom tools <adamowaissi055@gmail.com>
2026-08-20 01:01:53 +0300

    Create codec.py

---
+++ utils/codec.py
@@ -0,0 +1,10 @@
+instructionset = [
+    "Move", "LoadK", "LoadBool", "LoadNil", "GetUpval",
+    "GetGlobal", "GetTable", "SetGlobal", "SetUpval", "SetTable",
+    "NewTable", "Self", "Add", "Sub", "Mul", "Div", "Mod", "Pow",
+    "Unm", "Not", "Len", "Concat", "Jmp", "Eq", "Lt", "Le",
+    "Test", "TestSet", "Call", "TailCall", "Return", "ForLoop",
+    "ForPrep", "TForLoop", "SetList", "Close", "Closure", "VarArg"
+]
+
+instructioncategories = [0] * len(instructionset)
